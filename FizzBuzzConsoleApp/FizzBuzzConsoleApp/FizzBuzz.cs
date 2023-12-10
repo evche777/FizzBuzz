fe1e98bebe7fc96c2da9992c9977515fcc3379ad
Eva K <eva@evas-mbp.home>
2023-12-10 22:53:34 +0000

    cleaner solution with passing tests

--- FizzBuzzConsoleApp/FizzBuzzConsoleApp/FizzBuzz.cs
+++ FizzBuzzConsoleApp/FizzBuzzConsoleApp/FizzBuzz.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+
 namespace MathsTask
 {
     public class FizzBuzz
@@ -6,30 +8,26 @@ namespace MathsTask
         public List<string> execute(int x)
         {
             var output = new List<string>();
+            var builder = new StringBuilder();
+
             for (int y = 1; y <= x; y++)
             {
                 if (divisibleBy3(y))
                 {
-                    if (divisibleBy5(y))
-                    {
-                        output.Add("FizzBuzz");
-                    }
-                    else
-                    {
-                        output.Add("Fizz");
-                    }
+                    builder.Append("Fizz");
                 }
-                else
+                if (divisibleBy5(y))
                 {
-                    if (divisibleBy5(y))
-                    {
-                        output.Add("Buzz");
-                    }
-                    else
-                    {
-                        output.Add(y.ToString());
-                    }
+                    builder.Append("Buzz");
                 }
+
+                if(builder.Length == 0)
+                {
+                    builder.Append(y);
+                }
+
+                output.Add(builder.ToString());
+                builder.Clear();
             }
 
             return output;
