788ce6c64f3cca02b8ea8cf2d65e43ef227893c7
Eva K <eva@evas-mbp.home>
2023-12-10 22:05:59 +0000

    adding a proj structure

---
+++ FizzBuzzConsoleApp/FizzBuzzTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace FizzBuzzTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
